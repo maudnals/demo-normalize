71c84979944d249f6c1fa68228d09c70a56cc5ea
maudnals <maud.nalpas@gmail.com>
2018-10-23 00:16:32 +0200

    fix(img): dynamic import via require

--- src/artwork.js
+++ src/artwork.js
@@ -1,20 +1,32 @@
 import { imgToCanvasData } from "./utils/utils.canvas";
 import { saturation, hue, brightness, color } from "./utils/utils.pixels";
+const imgSrc = require("../assets/img/flashy.jpg");
 
 document.addEventListener("DOMContentLoaded", function(event) {
   // DOM fully loaded and parsed
-  var imgEl = document.getElementById("my-img");
-  const data = imgToCanvasData(imgEl);
 
-  const s = saturation(data);
-  console.log(s);
+  const img = new Image();
+  img.src = imgSrc;
 
-  const b = brightness(data);
-  console.log(b);
+  img.addEventListener("load", () => {
+    const imgEl = document.getElementById("my-img");
+    imgEl.src = imgSrc;
 
-  const h = hue(data);
-  console.log(h);
+    console.log("loaded");
+    const data = imgToCanvasData(imgEl);
 
-  const c = color(data);
-  console.log(c);
+    const s = saturation(data);
+    console.log(s);
+
+    const b = brightness(data);
+    console.log(b);
+
+    const h = hue(data);
+    console.log(h);
+
+    const c = color(data);
+    console.log(c);
+  });
 });
+
+document.addEventListener("calculationDone", function(event) {});
