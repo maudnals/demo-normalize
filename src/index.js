ffcea38dba08a358c3c7965da690b5e86dc27b4d
maudnals <maud.nalpas@gmail.com>
2019-09-01 21:44:28 +0200

    style(): edited

--- src/index.js
+++ src/index.js
@@ -1,8 +1,2 @@
-import './scene.js';
-import './artwork.js';
-
-// ts
-// redux for img etc
-// turn pixel functions into wasm via walt and rust
-// overview page
-// more data
+import "./scene.js";
+import "./artwork.js";
