b7ad83a6d55f73d8987934ae96069df2180c5fdd
maudnals <maud.nalpas@gmail.com>
2019-09-08 09:49:32 +0200

    data: cleaned up + added artworks info

--- src/data.js
+++ src/data.js
@@ -1,106 +1,86 @@
-// New data:
-// https://www.metmuseum.org/art/collection/search/459094?&searchField=All&when=A.D.+1900-present&ft=*&offset=240&rpp=80&amp;pos=262
-// https://www.metmuseum.org/art/collection/search/62511?&searchField=All&when=A.D.+1900-present&ft=*&offset=160&rpp=80&amp;pos=239
-// https://www.metmuseum.org/art/collection/search/437379?&searchField=All&when=A.D.+1900-present&ft=*&offset=160&rpp=80&amp;pos=224
-// https://www.metmuseum.org/art/collection/search/438822?&searchField=All&when=A.D.+1900-present&what=Paintings&ft=*&offset=80&rpp=80&amp;pos=85
-// https://www.metmuseum.org/art/collection/search/483434?&searchField=All&when=A.D.+1900-present&what=Paintings&ft=*&offset=0&rpp=20&amp;pos=9
-// https://www.metmuseum.org/art/collection/search/380531?&searchField=All&when=A.D.+1900-present&ft=*&offset=0&rpp=80&amp;pos=17
-// https://www.metmuseum.org/art/collection/search#!?showOnly=openAccess&offset=240&pageSize=0&sortOrder=asc&perPage=80&searchField=All&era=A.D.%201900-present
+import images from "../assets/imgs/*.jpg";
 
-// flowers: https://www.metmuseum.org/art/collection/search/44596?&searchField=All&sortBy=Relevance&when=A.D.+1900-present&ft=*&offset=80&rpp=80&amp;pos=118
+export const imgs = images;
 
-//kirche:
-// https://www.metmuseum.org/art/collection/search/647787?&searchField=All&sortBy=Relevance&when=A.D.+1900-present&ft=*&offset=0&rpp=80&amp;pos=35
+export const artworks = {
+  // keys are used to match the artwork image file with the artwork info
+  back: {
+    openUrl:
+      "https://www.metmuseum.org/art/collection/search/483434?&searchField=All&when=A.D.+1900-present&what=Paintings&ft=*&offset=0&rpp=20&amp;pos=9",
+    title: "Seated Woman, Back View",
+    year: "1917",
+    author: "Egon Schiele",
+    src: "MET"
+  },
+  gipsy: {
+    openUrl:
+      "https://www.metmuseum.org/art/collection/search/264528?&searchField=All&sortBy=Relevance&when=A.D.+1900-present&ft=*&offset=0&rpp=80&amp;pos=39",
+    title: "A Gypsy Dancing-Girl, Kathiawar",
+    year: "1915",
+    author: "E. Taurines",
+    src: "MET"
+  },
+  kirche: {
+    openUrl:
+      "https://www.metmuseum.org/art/collection/search/647787?&searchField=All&sortBy=Relevance&when=A.D.+1900-present&ft=*&offset=0&rpp=80&amp;pos=35",
+    title: "Vienna: St. Michael's Church (Wien: Die Michaelerkirche)",
+    year: "1908",
+    author: "Urban Janke",
+    src: "MET"
+  },
+  lion: {
+    openUrl:
+      "https://www.metmuseum.org/art/collection/search/438822?&searchField=All&when=A.D.+1900-present&what=Paintings&ft=*&offset=80&rpp=80&amp;pos=85",
+    title: "The Repast of the Lion",
+    year: "1907",
+    author: "Henri Rousseau (le Douanier)",
+    src: "MET"
+  },
+  moon: {
+    openUrl:
+      "https://www.metmuseum.org/art/collection/search/786297?&searchField=All&sortBy=Relevance&when=A.D.+1900-present&ft=*&offset=0&rpp=80&amp;pos=69",
+    title:
+      "Buzz Aldrin Walking on the Surface of the Moon Near a Leg of the Lunar Module",
+    year: "1969",
+    author: "Neil Armstrong",
+    src: "MET"
+  },
+  prisoner: {
+    openUrl:
+      "https://www.metmuseum.org/art/collection/search/55368?&searchField=All&sortBy=Relevance&when=A.D.+1900-present&ft=*&offset=0&rpp=80&amp;pos=71",
+    title: "Young Woman Looks at Silhouette of a Male Prisoner being Led Away",
+    year: "Early 20th century",
+    author: "Tomioka Eisen",
+    src: "MET"
+  },
+  south: {
+    openUrl:
+      "https://www.metmuseum.org/art/collection/search/459094?&searchField=All&when=A.D.+1900-present&ft=*&offset=240&rpp=80&amp;pos=262",
+    title: "Valley with Fir (Shade on the Mountain)",
+    year: "1909",
+    author: "Henri-Edmond Delacroix",
+    src: "MET"
+  },
+  sumer: {
+    openUrl:
+      "https://www.metmuseum.org/art/collection/search/327394?&searchField=All&sortBy=Relevance&when=8000-2000+B.C.&ft=*&offset=640&rpp=80&amp;pos=652",
+    title: "Foundation figure of a deity",
+    year: "ca. 2500–2350 B.C",
+    author: "Sumerian",
+    src: "MET"
+  },
+  train: {
+    openUrl:
+      "https://www.metmuseum.org/art/collection/search/380531?&searchField=All&when=A.D.+1900-present&ft=*&offset=0&rpp=80&amp;pos=17",
+    title: "Exactitude",
+    year: "1929",
+    author: "Pierre Fix-Masseau",
+    src: "MET"
+  }
+};
 
-// gipsy: https://www.metmuseum.org/art/collection/search/264528?&searchField=All&sortBy=Relevance&when=A.D.+1900-present&ft=*&offset=0&rpp=80&amp;pos=39
+export const imgIds = Object.keys(imgs);
 
-// japan sea: https://www.metmuseum.org/art/collection/search/55612?&searchField=All&sortBy=Relevance&when=A.D.+1900-present&ft=*&offset=0&rpp=80&amp;pos=53
+export const info = imgIds.map(id => artworks[id]);
 
-// prisoner https://www.metmuseum.org/art/collection/search/55368?&searchField=All&sortBy=Relevance&when=A.D.+1900-present&ft=*&offset=0&rpp=80&amp;pos=71
-
-// buzz
-// https://www.metmuseum.org/art/collection/search/786297?&searchField=All&sortBy=Relevance&when=A.D.+1900-present&ft=*&offset=0&rpp=80&amp;pos=69
-
-// prisoner https://www.metmuseum.org/art/collection/search/55368?&searchField=All&sortBy=Relevance&when=A.D.+1900-present&ft=*&offset=0&rpp=80&amp;pos=71
-
-// export default imgs = [
-//   {
-//     src: require("../assets/img/flashy.jpg"),
-//     title:
-//       "Three Color Curl (CMY: Irvine, California, August 25th 2008, Fuji Crystal Archive Type C)",
-//     artist: "Walead Beshty",
-//     dateCreated: "2008",
-//     medium: "chromogenic print",
-//     dimensionsCm: "249.56 x 127",
-//     copyright: "© Walead Beshty",
-//     permanentURL: "https://www.sfmoma.org/artwork/2010.101",
-//     status: "Not on view at this time"
-//   },
-//   require("../assets/img/flashy.jpg"),
-//   require("../assets/img/photo.jpg"),
-//   require("../assets/img/liquor.jpg"),
-//   require("../assets/img/tree.jpg"),
-//   require("../assets/img/cakes.jpg"),
-//   require("../assets/img/tree-pink.jpg"),
-//   require("../assets/img/squares.jpg")
-// ];
-
-// <!-- Artwork Info
-// Artwork title
-// Untitled #40, from the series I, Tokyo
-// Artist name
-// Jacob Aue Sobol
-// Date created
-// 2006-2008
-// Classification
-// photograph
-// Medium
-// gelatin silver print
-// Dimensions
-// 16 in. x 20 in. (40.64 cm x 50.8 cm)
-// Date acquired
-// 2010
-// Credit
-// Collection SFMOMA
-// Accessions Committee Fund purchase
-// Copyright
-// © Jacob Aue Sobol
-// Permanent URL
-// https://www.sfmoma.org/artwork/2010.203
-// Artwork status
-// Not on view at this time. -->
-
-// <!-- <button id="my-button">Change image</button> -->
-
-//     <!-- liquor: https://www.sfmoma.org/artwork/2017.48.3.A-B -->
-
-//     <!-- tree: https://www.sfmoma.org/artwork/2010.197 -->
-
-//     <!-- cakes: https://www.sfmoma.org/artwork/73.52 -->
-
-//     <!-- tree pink: https://www.sfmoma.org/artwork/2017.73 -->
-//     <!-- squares: https://www.sfmoma.org/artwork/87.88 -->
-
-//     <!-- <img src="/assets/mondrian.jpeg" alt="">
-//   <img src="./assets/mondrian.jpeg" alt=""> -->
-//     <!-- <img id="my-img" src="https://s3-us-west-2.amazonaws.com/sfmomamedia/media/t/collection_images/fdnT5DEwDhLp.jpg" alt=""> -->
-
-//     <!-- <img src="./assets/mondrian.jpeg" alt=""> -->
-//     <!-- <script src="./index.js"></script> -->
-//     <!--  -->
-//     <!-- hello -->
-
-//     <!-- flashy: Artwork Info
-//   Artwork title
-
-//   Medium
-//   chromogenic print
-//   Dimensions
-//   98 1/4 in. x 50 in. (249.56 cm x 127 cm)
-
-//   Copyright
-//   © Walead Beshty
-//   Permanent URL
-//   https://www.sfmoma.org/artwork/2010.101
-//   Artwork status
-//   Not on view at this time. --></img>
+// Picture I liked: https://www.sfmoma.org/artwork/2010.203
