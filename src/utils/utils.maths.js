945ad99eaa72d15ad5729d21e9370bac989a0a75
maudnals <maud.nalpas@gmail.com>
2019-09-08 09:59:28 +0200

    feat: new util for wireframe

--- src/utils/utils.maths.js
+++ src/utils/utils.maths.js
@@ -7,3 +7,5 @@ const sum = (a, b) => {
 export const average = arr => {
   return arr.filter(withoutNaNs).reduce(sum, 0) / arr.length;
 };
+
+export const randomBoolean = () => !!Math.round(Math.random());
